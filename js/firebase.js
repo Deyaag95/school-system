37c5c295a1984960918aeb0a86b707a1e593604e
Deyaag95 <deyaag1995@gmail.com>
2025-12-05 01:45:01 +0200

    Refactor Firebase initialization and config
    Updated Firebase imports and configuration details.

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,21 @@
-// js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB-0JJr4habltXLo7JMGNrmfnlrD8H_k1E",
+  authDomain: "school-system-46cce.firebaseapp.com",
+  projectId: "school-system-46cce",
+  storageBucket: "school-system-46cce.firebasestorage.app",
+  messagingSenderId: "938854271066",
+  appId: "1:938854271066:web:81b8399642b918bc36b063",
+  measurementId: "G-RK9QXE1RJS"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
